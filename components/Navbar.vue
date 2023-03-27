4bbc7a02f3b124c9b409a50190c48e4cce857b39
adisamicheal <adisamicheal20@gmail.com>
2023-03-27 22:30:06 +0100

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <header class="header">
+    <div class="logo">Offside</div>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Docs</li>
+            <li>Matches</li>
+        </ul>
+    </nav>
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+.header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 50px 100px;
+    .logo {
+        font-size: 36px;
+    }
+    nav {
+        ul {
+            list-style: none;
+            display: flex;
+            gap: 20px;
+        }
+    }
+}
+</style>
