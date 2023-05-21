7cbdf19ccd32768e26359b04be7013358fb6fd8c
adisamicheal <adisamicheal20@gmail.com>
2023-05-21 22:02:57 +0100

    landing page fixtures

---
+++ components/LandingUpcomingFixtures/LandingUpcomingFixtures.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="upcoming-fixtures">
+        <div class="upcoming-fixtures__header">
+          <h1>Upcoming Fixtures</h1>
+          <p>Make your predictions, rack up points.</p>
+        </div>
+        <div class="upcoming-fixtures__tabs">
+            <div class="upcoming-fixtures__tabs__tab english">
+              <div class="english_image"></div>
+              <p>English Premier League</p>
+            </div>
+            <div
+              class="upcoming-fixtures__tabs__tab champions"
+              @mouseover="championsLeagueIsHoverState = true"
+              @mouseleave="championsLeagueIsHoverState = false">
+              <div class="champions_image" :style="{ backgroundImage: `url(${backgroundUrl})` }"></div>
+              <p>UEFA Champions League</p>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+let championsLeagueIsHoverState = ref(false)
+let championsLeagueIsActiveState = ref(false)
+
+let backgroundUrl = ref('../../assets/icons/champions-league-inactive.svg')
+
+watch(championsLeagueIsHoverState, (val) => {
+  if(val === true) {
+    console.log('hover');
+    backgroundUrl.value = '../../assets/icons/champions-league-hover.svg';
+  } else if (val === false) (
+    backgroundUrl.value = '../../assets/icons/champions-league-inactive.svg'
+  )
+}, { immediate: true })
+
+</script>
+
+<style src="./LandingUpcomingFixtures.scss"></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,117 @@
-<script setup lang="ts">
-</script>
 <template>
   <div>
-    Offside
+    <div class="home__week__card">
+      <div class="home__week__card--flex">
+        <div class="home__week__card--flex__left">
+          <p class="fl">Total prize this week</p>
+        </div>
+        <div class="home__week__card--flex__right">
+          <p class="fr">{{ homeState.weekPrice }}</p>
+        </div>
+      </div>
+      <div class="home__week__card--flex middle">
+        <div class="home__week__card--flex__left">
+          <p class="fr">{{ homeState.weekStake }} stakes.</p>
+        </div>
+        <div class="home__week__card--flex__right">
+          <p class="fl">Total stakes</p>
+        </div>
+      </div>
+      <div class="home__week__card--flex">
+        <div class="home__week__card--flex__left">
+          <p class="fl">Prediction deadline</p>
+        </div>
+        <div class="home__week__card--flex__right">
+          <p class="fr">{{ homeState.weekDeadline }}.</p>
+        </div>
+      </div>
+    </div>
+    <!-- <LandingUpcomingFixtures /> -->
+    <Footer />
   </div>
-  <Footer />
 </template>
 
-<style lang="scss">
+<script setup lang="ts">
 
+const homeState: HomeState = {
+  weekPrice: 5000000.00,
+  weekStake: 16,
+  weekDeadline: '1pm, Thursday, June 2023'
+}
+
+</script>
+
+<style lang="scss">
+.home {
+  &__week {
+    &__card {
+      .middle {
+        flex-direction: row-reverse;
+        @media screen and (max-width: 768px) {
+          flex-direction: column;
+        }
+      }
+      .fl {
+        font-weight: 600;
+        font-size: 48px;
+        line-height: 48px;
+        @media screen and (max-width: 768px) {
+          font-size: 24px;
+          line-height: 24px;
+        }
+      }
+      .fr {
+        font-weight: 800;
+        font-size: 64px;
+        line-height: 64px;
+        @media screen and (max-width: 768px) {
+          font-size: 32px;
+          line-height: 32px;
+        }
+      }
+      &--flex {
+        display: flex;
+        justify-content: space-between;
+        @media screen and (max-width: 768px) {
+          flex-direction: column;
+        }
+        &__left{
+          min-height: 324px;
+          width: 50%;
+          display: flex;
+          align-items: center;
+          background: #000;
+          color: #fff;
+          padding: 64px;
+          @media screen and (max-width: 768px) {
+            min-height: 150px;
+            width: 100%;
+            padding: 20px;
+            justify-content: center;
+            text-align: center;
+          }
+        }
+        &__right {
+          min-height: 324px;
+          width: 50%;
+          display: flex;
+          align-items: center;
+          background: #fff;
+          color: #000;
+          padding: 64px;
+          @media screen and (max-width: 768px) {
+            min-height: 150px;
+            width: 100%;
+            padding: 20px;
+            justify-content: center;
+            text-align: center;
+          }
+          p {
+            max-width: 455px;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
