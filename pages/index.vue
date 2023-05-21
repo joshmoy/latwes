f5a002b2c52e499858c8ee9d6e0feac5bc3802e0
adisamicheal <adisamicheal20@gmail.com>
2023-05-21 22:56:34 +0100

    landing page fixtures

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,4 @@
   <Footer />
 </template>
 
-<script setup lang="ts">
-
-const homeState: HomeState = {
-  weekPrice: 5000000.00,
-  weekStake: 16,
-  weekDeadline: '1pm, Thursday, June 2023'
-}
-
-</script>
-
 <style lang="scss"></style>
