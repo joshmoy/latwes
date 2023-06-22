d679eca6fe5a53da40f59e8f2934b15ecb44e1ae
adisamicheal <adisamicheal20@gmail.com>
2023-06-22 15:40:37 +0100

    prediction-cards

--- components/DateScroll/DateScroll.vue
+++ components/DateScroll/DateScroll.vue
@@ -1,7 +1,87 @@
 <template>
-  
+  <div class="fixtures">
+    <div class="fixtures__left" @click=slide(-1)>
+        <svgo-match-arrrow />
+    </div>
+    <div class="fixtures__matches" id="match-fixtures">
+      <div
+        
+        class="fixtures__matches--match"
+        :class="{'fixtures__matches--match__active': Number(match.id) === Number(activeMatch && activeMatch.id)}"
+        v-for="match in displayValues()"
+        :key="match.id"
+        @click="selectMatch(match.id)">
+        <p class="matchday">Mathday {{ match.match_day }}</p>
+        <p class="points">{{ match.points }} pts</p>
+        <!-- <p class="matchdate">{{ match.start_at }}</p> -->
+      </div>
+    </div>
+    <div class="fixtures__right" @click="slide(1)">
+        <svgo-match-arrrow />
+    </div>
+  </div>
 </template>
 
-<script  setup lang="ts"></script>
+<script  setup lang="ts">
+import { ref, computed } from 'vue';
+import Events from '@/assets/mockData/Events.json'
+
+const currentIndex = ref(0)
+const direction = ref(1)
+const activeMatchesShown = ref(Events.data.events.slice(currentIndex.value, currentIndex.value + 9))
+const activeMatch = ref()
+
+const matches = computed(() => {
+  return Events.data.events;
+})
+const displayValues = () => {
+  const visibleMatches = matches.value.slice(currentIndex.value, currentIndex.value + 9);
+  return visibleMatches;
+}
+const triggerSliderAnimation = (val: number) => {
+  console.log('called?');
+  
+  const sliderElement:any = document.getElementById('match-fixtures');
+  if (val === 1) {
+    // Trigger sliding animation
+    sliderElement.classList.add('slide-animation-to');
+    
+    // Remove sliding animation after it completes
+    setTimeout(() => {
+      sliderElement.classList.remove('slide-animation-to');
+    }, 150);
+  } else if (val === -1) {
+    console.log('that');
+    
+    // Trigger sliding animation
+    sliderElement.classList.add('slide-animation-from');
+    
+    // Remove sliding animation after it completes
+    setTimeout(() => {
+      sliderElement.classList.remove('slide-animation-from');
+    }, 150);
+  }
+}
+const slide = (val: number) => {
+  if (val === 1) {
+    if (currentIndex.value + 9 < matches.value.length) {
+      currentIndex.value++;
+      displayValues();
+      triggerSliderAnimation(val);
+    }
+  } else if (val === -1) {
+    if (currentIndex.value !== 0 && currentIndex.value + 9 <= matches.value.length) {
+      currentIndex.value--;
+      displayValues();
+      triggerSliderAnimation(val);
+    }
+  }
+}
+
+const selectMatch = async (id: number) => {
+  const active = await activeMatchesShown.value.find(val => val.id === id)
+  activeMatch.value = active
+}
+</script>
 
 <style lang="scss" scoped src="./DateScroll.scss"></style>
---
+++ components/PredictionCard/PredictionCard.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="prediction-card">
+        <div class="prediction-card__teams">
+            <p class="prediction-card__teams--heading">Enter your prediction</p>
+            <div class="prediction-card__teams--body">
+                <div class="team-input__a">
+                    <div class="team-input__a__color logo"></div>
+                    <p>Bolton</p>
+                </div>
+                <div class="team-prediction">
+                    <div class="team-input__a__prediction">
+                        <div v-if="homeTeamPredictionActive">
+                            <input type="text">
+                        </div>
+                        <div v-else class="plus">
+                            <svgo-prediction-plus />
+                        </div>
+                    </div>
+                    <p class="column">:</p>
+                    <div class="team-input__b__prediction">
+                        <div v-if="awayTeamPredictionActive">
+                            <input type="text">
+                        </div>
+                        <div v-else class="plus">
+                            <svgo-prediction-plus />
+                        </div>
+                    </div>
+                </div>
+                <div class="team-input__b">
+                    <div class="team-input__b__color logo"></div>
+                    <p>Bolton</p>
+                </div>
+            </div>
+        </div>
+        <div class="points">
+            4pts
+        </div>
+        <div class="prediction-card__stats">
+            <div class="prediction-card__stats--left">
+                <p class="top">Top predictions</p>
+                <div class="scores">
+                    <div>
+                        <p class="score">2-1</p>
+                        <p class="percent">30%</p>
+                    </div>
+                    <div>
+                        <p class="score">2-1</p>
+                        <p class="percent">30%</p>
+                    </div>
+                    <div>
+                        <p class="score">2-1</p>
+                        <p class="percent">30%</p>
+                    </div>
+                </div>
+            </div>
+            <div class="prediction-card__stats--right">
+                <p class="kick-off">Kick off</p>
+                <p class="date">Wed May 10, 4:00pm</p>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const homeTeamPredictionActive = ref(false);
+const awayTeamPredictionActive = ref(false);
+
+</script>
+
+<style lang="scss" scoped src="./PredictionCard.scss"></style>
