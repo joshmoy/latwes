d23afa40cd2e845cc1c99bd0484e485948d16a7a
joshmoy <obodaij@gmail.com>
2023-08-05 21:37:36 +0100

    ft/implement dashboard header

---
+++ layouts/DashboardLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="dashboard-layout">
+   <DashboardNav />
+    <!-- Content slot to inject the page-specific content -->
+    <main class="container">
+      <p>tesr</p>
+      <slot />
+    </main>
+
+    <!-- Add your common footer or other elements here -->
+    <footer>
+      <!-- Your footer content -->
+    </footer>
+  </div>
+</template>
+<!-- 
+<style lang="scss" scoped src="./DashboardLayout.scss"></style> -->
+
---
+++ pages/dashboard/competitions.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLayout name="dashboard-layout">
+    <div>
+      <h1>Dashboard Page 1</h1>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script></script>
