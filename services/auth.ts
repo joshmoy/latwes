bef2abf49035bcff2bcc3231c9247409dcc9e551
joshmoy <obodaij@gmail.com>
2023-08-09 21:45:49 +0100

    chore/add unauthenticated axios

--- services/auth.ts
+++ services/auth.ts
@@ -1,13 +1,13 @@
-import axiosInstance from ".";
+import { axiosWithoutAuth } from ".";
 
 export const signUpService = (payload: Record<string, string>) =>
-  axiosInstance.post("/auth/signup", payload);
+  axiosWithoutAuth.post("/auth/signup", payload);
 
 export const logInService = (payload: Record<string, string>) =>
-  axiosInstance.post("/auth/login", payload);
+  axiosWithoutAuth.post("/auth/login", payload);
 
 export const forgotPasswordService = (payload: Record<string, string>) =>
-  axiosInstance.post("/auth/forgot-password", payload);
+  axiosWithoutAuth.post("/auth/forgot-password", payload);
 
 export const resetPasswordService = (payload: Record<string, string>) =>
-  axiosInstance.post("/auth/set-password", payload);
+  axiosWithoutAuth.post("/auth/set-password", payload);
