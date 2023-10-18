55c00ed4e18f328ca0cf28cb66dbc06faf36ff19
joshmoy <obodaij@gmail.com>
2023-10-18 20:44:12 +0100

    ft: implement about us page

---
+++ components/AboutHeader/AboutHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="hero">
+    <Navbar :home-nav="true" />
+    <section class="hero-header">
+      <div class="container hero-header-container">
+        <h1 class="hero-header-title">About <span>Us</span></h1>
+      </div>
+    </section>
+    <div class="hero__icons">
+      <img src="../../assets/icons/hero-left.svg" alt="" />
+      <img src="../../assets/icons/hero-right.svg" alt="" />
+    </div>
+  </main>
+</template>
+<style lang="scss" scoped src="./AboutHeader.scss"></style>
