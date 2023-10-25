d96f003e74799bc47f77066d6e3e02490fb25c5e
joshmoy <obodaij@gmail.com>
2023-10-25 16:01:50 +0100

    ft: how to play

---
+++ components/PointsComponent/PointsComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="right">
+    <div
+      class="points"
+      :class="{
+        'points-lemon': pointsEarned === 6,
+        'points-orange': pointsEarned > 0 && pointsEarned < 6,
+      }"
+    >
+      <p>
+        {{ pointsEarned === 1 ? "1pt" : pointsEarned === null ? 0 + "pts" : pointsEarned + "pts" }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface IHeroProps {
+  pointsEarned: number;
+}
+
+defineProps<IHeroProps>();
+</script>
+<style lang="scss" scoped src="./PointsComponent.scss"></style>
+
