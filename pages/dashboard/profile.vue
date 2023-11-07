0eeccdd7aea8b58fa8aec47607906cc818064dbb
adisamicheal <adisamicheal20@gmail.com>
2023-11-07 19:31:34 +0000

    fix logout on mobile and og card

--- pages/dashboard/profile.vue
+++ pages/dashboard/profile.vue
@@ -46,23 +46,4 @@ onBeforeMount(async () => {
 useHead({
   title: "Profile",
 });
-
-useSeoMeta({
-  title: "Goborr",
-  ogTitle: "Goborr: Dive into the Ultimate Soccer Prediction Challenge!",
-  description:
-    "Join the Goborr community – the pinnacle of soccer predicting platforms! Test your sports acumen, pit against fellow enthusiasts, and stand a chance to win big every week.",
-  ogDescription:
-    "Predict. Compete. Win. Goborr offers you a front-row seat to the world of soccer forecasting. Rise up the ranks, and grab your weekly jackpot! Ready for the challenge?",
-  ogImage:
-    "https://res.cloudinary.com/demp/image/upload/v1699383189/Compete_With_io6fec.jpg",
-  twitterImage:
-    "https://res.cloudinary.com/demp/image/upload/v1699383189/Compete_With_io6fec.jpg",
-  twitterCard: "summary_large_image",
-  twitterTitle: "Goborr",
-  twitterDescription:
-    "Predict. Compete. Win. Goborr offers you a front-row seat to the world of soccer forecasting. Rise up the ranks, and grab your weekly jackpot! Ready for the challenge?",
-  charset: "UTF-8",
-  viewport: "width=device-width, initial-scale=1.0",
-});
 </script>
