36dcccb8ead99e49adb4f083f305d33406f126c2
joshmoy <obodaij@gmail.com>
2023-12-13 14:57:41 +0100

    ft: update prediction card view

--- components/ViewPredictionBannner/ViewPredictionBanner.vue
+++ components/ViewPredictionBannner/ViewPredictionBanner.vue
@@ -2,12 +2,31 @@
   <div class="pool-banner">
     <div class="pool-banner-flex">
       <div class="pool-banner-text">
-        <p class="pool-banner-text-title">Match Details</p>
+        <p class="pool-banner-text-title">{{ fixture?.has_finished ? 'Match Ended' : 'Live Score' }}</p>
         <p>{{ fixture?.home_team }} {{ fixture?.home_team_score }} - {{ fixture?.away_team_score }} {{ fixture?.away_team }}</p>
       </div>
     </div>
     <div class="pool-banner-flex">
-      <img src="/icons/newPool.svg" class="second-image" />
+       <div class="pool-banner-flex--right">
+          <p class="kick-off">Points earned</p>
+          <div
+            class="points"
+            :class="{
+              'points-lemon': fixture?.score === 6,
+              'points-orange': fixture?.score > 0 && fixture?.score < 6,
+            }"
+          >
+            <p>
+              {{
+                fixture?.score === 1
+                  ? "1pt"
+                  : fixture?.score === null
+                  ? 0 + "pts"
+                  : fixture?.score + "pts"
+              }}
+            </p>
+          </div>
+        </div>
     </div>
   </div>
 </template>
